189b0789631fbc5ec2321e591d210a641703acac
M.P. Korstanje <rien.korstanje@gmail.com>
2026-01-13 14:39:54 +0100

    Revert "Introduce module"
    This reverts commit a5c624c474d75f5cc1eeccd354fdee58fdce5df7.

--- java/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module io.cucumber.teamcityformatter {
-    requires org.jspecify;
-    
-    requires io.cucumber.messages;
-    requires io.cucumber.query;
-
-    exports io.cucumber.teamcityformatter;
-}
