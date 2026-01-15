b07796093cad7fe919cc9800bb899ba3639f3e85
M.P. Korstanje <rien.korstanje@gmail.com>
2026-01-15 21:33:57 +0100

    Transitively require messages

--- java/src/main/java/module-info.java
+++ java/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module io.cucumber.teamcityformatter {
     requires org.jspecify;
     
-    requires io.cucumber.messages;
+    requires transitive io.cucumber.messages;
     requires io.cucumber.query;
 
     exports io.cucumber.teamcityformatter;
